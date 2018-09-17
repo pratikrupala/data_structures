1178b85595c85bc4aff4e8dc64475b7bc594cbd9
Pratik Rupala <prtrpl@gmail.com>
2018-09-17 01:33:33 -0700

    Added a tree structure support for nodes having link to their parents

---
+++ tree/practice/headers/tree_parent_pointer.h
@@ -0,0 +1,47 @@
+#define MAX_LEN	8
+
+struct tree {
+	struct tree *left;
+	struct tree *right;
+	struct tree *parent;
+	int data;
+};
+
+int
+insert(struct tree *p, int data)
+{
+	if (!p) {
+		printf("Parent is NULL.\n");
+		return -1;
+	}
+
+	if (!(p->left)) {
+		p->left = (struct tree *) malloc(sizeof(struct tree));
+		if (!(p->left)) {
+			printf("Failed to allocate memory for data"
+					" %d\n", data);
+			return -1;
+		}
+		p->left->left = NULL;
+		p->left->right = NULL;
+		p->left->parent = p;
+		p->left->data = data;
+	} else if (!(p->right)) {
+		p->right = (struct tree *) malloc(sizeof(struct tree));
+		if (!(p->right)) {
+			printf("Failed to allocate memory for data"
+					" %d\n", data);
+			return -1;
+		}
+		p->right->left = NULL;
+		p->right->right = NULL;
+		p->right->parent = p;
+		p->right->data = data;
+	} else {
+		printf("\nInvalid condition. Both the child of the parent %d"
+				" is already present\n", p->data);
+		return -1;
+	}
+
+	return 0;
+}
